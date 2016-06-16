5762a1ac560d9238f8379b37e71b5f2ae5ce79f8
camp-vfa <vikfand@gmail.com>
2016-06-16 10:29:16 +0200

    Make simple proxy server

---
+++ proxy-proxy/src/main/java/no/difi/idporten/oidc/proxy/proxy/ProxyFrontendHandler.java
@@ -0,0 +1,76 @@
+package no.difi.idporten.oidc.proxy.proxy;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+
+public class ProxyFrontendHandler extends ChannelInboundHandlerAdapter {
+    private final String remoteHost;
+    private final int remotePort;
+
+    private volatile Channel outboundChannel;
+
+    public ProxyFrontendHandler(String remoteHost, int remotePort) {
+        this.remoteHost = remoteHost;
+        this.remotePort = remotePort;
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        final Channel inboundChannel = ctx.channel();
+
+        Bootstrap b = new Bootstrap();
+        b.group(inboundChannel.eventLoop())
+                .channel(ctx.channel().getClass())
+                .handler(new ProxyServerHandler(inboundChannel))
+                .option(ChannelOption.AUTO_READ, false);
+        ChannelFuture f = b.connect(this.remoteHost, this.remotePort);
+        outboundChannel = f.channel();
+
+        f.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                if (channelFuture.isSuccess()) {
+                    inboundChannel.read();
+                } else {
+                    inboundChannel.close();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        if (outboundChannel.isActive()) {
+            outboundChannel.writeAndFlush(msg).addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                    if (channelFuture.isSuccess()) {
+                        ctx.channel().read();
+                    } else {
+                        channelFuture.channel().flush();
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) {
+        if (outboundChannel != null) {
+            closeOnFlush(ctx.channel());
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        closeOnFlush(outboundChannel);
+    }
+
+    static void closeOnFlush(Channel ch) {
+        if (ch.isActive()) {
+            ch.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+        }
+    }
+}
--- proxy-proxy/src/main/java/no/difi/idporten/oidc/proxy/proxy/ProxyServerHandler.java
+++ proxy-proxy/src/main/java/no/difi/idporten/oidc/proxy/proxy/ProxyServerHandler.java
@@ -1,43 +1,42 @@
 package no.difi.idporten.oidc.proxy.proxy;
 
-import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.handler.codec.http.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import io.netty.channel.*;
 
-public class ProxyServerHandler extends SimpleChannelInboundHandler<Object> {
+public class ProxyServerHandler extends ChannelInboundHandlerAdapter {
 
-    private static Logger logger = LoggerFactory.getLogger(ProxyServerHandler.class);
+    private final Channel inboundChannel;
 
-    private HttpRequest httpRequest;
-
-    @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        ctx.flush();
+    public ProxyServerHandler(Channel inboundChannel) {
+        this.inboundChannel = inboundChannel;
     }
 
     @Override
-    protected void channelRead0(ChannelHandlerContext context, Object msg) throws Exception {
-        logger.info("Received ({}): {}", msg.getClass(), msg);
+    public void channelActive(ChannelHandlerContext ctx) {
+        ctx.read();
+    }
 
-        if (msg instanceof HttpRequest) {
-            httpRequest = (HttpRequest) msg;
-        } else if (msg instanceof HttpContent) {
-            if (msg instanceof LastHttpContent) {
-                FullHttpResponse httpResponse = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
-                httpResponse.content().writeBytes(String.format("Hello %s!", httpRequest.uri()).getBytes());
-
-                context.write(httpResponse);
-                context.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    @Override
+    public void channelRead(final ChannelHandlerContext ctx, Object msg) {
+        inboundChannel.writeAndFlush(msg).addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                if (channelFuture.isSuccess()) {
+                    ctx.channel().read();
+                } else {
+                    channelFuture.channel().close();
+                }
             }
-        }
+        });
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        logger.error(cause.getMessage(), cause);
+    public void channelInactive(ChannelHandlerContext ctx) {
+        ProxyFrontendHandler.closeOnFlush(inboundChannel);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
     }
 }
