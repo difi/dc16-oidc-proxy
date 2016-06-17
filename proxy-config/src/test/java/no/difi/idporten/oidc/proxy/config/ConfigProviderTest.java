553ff62398a4bdb60dffb08ff6335889b9bafda4
NoraTomas <noratomas@hotmail.com>
2016-06-17 13:46:58 +0200

    Added test to ConfigProviderTest

--- proxy-config/src/test/java/no/difi/idporten/oidc/proxy/config/ConfigProviderTest.java
+++ proxy-config/src/test/java/no/difi/idporten/oidc/proxy/config/ConfigProviderTest.java
@@ -10,18 +10,18 @@ import java.util.ArrayList;
 public class ConfigProviderTest {
 
     public static void main(String [] args){
-        ArrayList<Host> levelFourhosts = new ArrayList<>(); //Hosts needing level 4 clearance
+        ArrayList<Host> levelFourHosts = new ArrayList<>(); //Hosts needing level 4 clearance
         ArrayList<String> securePaths = new ArrayList<>(); //Paths needing level 4 clearance
 
         Host host1 = new Host("default/test/");
         Host host2 = new Host("default2");
 
         securePaths.add("/test/");
-        levelFourhosts.add(host1);
+        levelFourHosts.add(host1);
 
-        StandardConfigProvider standardConfigProvider1 = new StandardConfigProvider(levelFourhosts, securePaths);
-        StandardConfigProvider standardConfigProvider2 = new StandardConfigProvider(levelFourhosts, securePaths);
-        StandardConfigProvider standardConfigProvider3 = new StandardConfigProvider(levelFourhosts, securePaths);
+        StandardConfigProvider standardConfigProvider1 = new StandardConfigProvider(levelFourHosts, securePaths);
+        StandardConfigProvider standardConfigProvider2 = new StandardConfigProvider(levelFourHosts, securePaths);
+        StandardConfigProvider standardConfigProvider3 = new StandardConfigProvider(levelFourHosts, securePaths);
 
         //Test 1
         if (standardConfigProvider1.needsLevelFour(host1) == true){
@@ -40,11 +40,19 @@ public class ConfigProviderTest {
         }
 
         //Test 3
-        if(standardConfigProvider3.pathRequirement("/test/")){
+        if(standardConfigProvider3.pathRequirement("/test/") == true){
             System.out.println("Test 3 success");
         }
         else{
             System.out.println("Test 3 failed");
+        }
+
+        //Test 4
+        if(standardConfigProvider3.pathRequirement("/test2") == false){
+            System.out.println("Test 4 success");
+        }
+        else{
+            System.out.println("Test 4 failed");
         }
 
 
