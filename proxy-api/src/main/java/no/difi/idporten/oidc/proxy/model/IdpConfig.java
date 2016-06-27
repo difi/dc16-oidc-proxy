2d54a2745fcba295a59c02d1695bb1f4424cde12
NoraTomas <noratomas@hotmail.com>
2016-06-27 08:15:27 +0200

    Started reading from config-file

--- proxy-api/src/main/java/no/difi/idporten/oidc/proxy/model/IdpConfig.java
+++ proxy-api/src/main/java/no/difi/idporten/oidc/proxy/model/IdpConfig.java
@@ -1,4 +1,24 @@
 package no.difi.idporten.oidc.proxy.model;
 
+import java.util.HashMap;
+
 public class IdpConfig {
-}
+
+    private String idpclass;
+    private String client_id;
+    private String password;
+    private String scope;
+    private String redirect_uri;
+    private HashMap<String, String> parameters;
+
+    public IdpConfig(String idpclass, String client_id, String password, String scope, String redirect_uri, HashMap<String, String> parameters){
+        this.idpclass=idpclass;
+        this.client_id=client_id;
+        this.password=password;
+        this.scope=scope;
+        this.redirect_uri=redirect_uri;
+        this.parameters = parameters;
+    }
+
+
+};
--- proxy-config/src/main/java/no/difi/idporten/oidc/proxy/config/TypesafeIdpConfigProvider.java
+++ proxy-config/src/main/java/no/difi/idporten/oidc/proxy/config/TypesafeIdpConfigProvider.java
@@ -2,14 +2,33 @@ package no.difi.idporten.oidc.proxy.config;
 
 import com.google.inject.Inject;
 import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import no.difi.idporten.oidc.proxy.api.IdpConfigProvider;
+import no.difi.idporten.oidc.proxy.model.AccessRequirement;
 import no.difi.idporten.oidc.proxy.model.IdpConfig;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TypesafeIdpConfigProvider implements IdpConfigProvider {
+    Config idpConfig;
 
     @Inject
     public TypesafeIdpConfigProvider(Config config) {
+        idpConfig = config;
+
+
         // TODO Initiate cache.
+    }
+
+    public void run(){
+
+        for (String key : idpConfig.getObject("idp").keySet()) {
+            System.out.println(idpConfig.getStringList("idp"));
+            //List<String> idpFields =  idpConfig.getStringList(String.format("idp.%s", key));
+
+        }
+
     }
 
     @Override
@@ -17,4 +36,10 @@ public class TypesafeIdpConfigProvider implements IdpConfigProvider {
         // TODO Implement this.
         return null;
     }
+
+    public static void main(String[] args) {
+        new TypesafeIdpConfigProvider(ConfigFactory.load()).run();
+    }
+
+
 }
