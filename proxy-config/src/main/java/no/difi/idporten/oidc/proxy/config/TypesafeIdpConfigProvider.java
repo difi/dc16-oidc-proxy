405e788a9552d18da038b7304f9854ca360fd6d0
NoraTomas <noratomas@hotmail.com>
2016-06-27 09:00:44 +0200

    Checking output from config file

--- proxy-config/src/main/java/no/difi/idporten/oidc/proxy/config/TypesafeIdpConfigProvider.java
+++ proxy-config/src/main/java/no/difi/idporten/oidc/proxy/config/TypesafeIdpConfigProvider.java
@@ -3,12 +3,15 @@ package no.difi.idporten.oidc.proxy.config;
 import com.google.inject.Inject;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigValue;
 import no.difi.idporten.oidc.proxy.api.IdpConfigProvider;
 import no.difi.idporten.oidc.proxy.model.AccessRequirement;
 import no.difi.idporten.oidc.proxy.model.IdpConfig;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TypesafeIdpConfigProvider implements IdpConfigProvider {
     Config idpConfig;
@@ -22,12 +25,18 @@ public class TypesafeIdpConfigProvider implements IdpConfigProvider {
     }
 
     public void run(){
+        System.out.println(idpConfig.getStringList("idp.idporten"));
+//        for (HashMap.Entry<String, Object> s:idpConfig.getObject("idp").unwrapped()){
+//
+//        }
 
-        for (String key : idpConfig.getObject("idp").keySet()) {
-            System.out.println(idpConfig.getStringList("idp"));
-            //List<String> idpFields =  idpConfig.getStringList(String.format("idp.%s", key));
-
-        }
+//        System.out.println(idpConfig.getObject("idp"));
+//
+//        for (String key : idpConfig.getObject("idp").keySet()) {
+//            System.out.println(idpConfig.getObject("idp"));
+//            //List<String> idpFields =  idpConfig.getStringList(String.format("idp.%s", key));
+//
+//        }
 
     }
 
