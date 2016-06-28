d6709f644cce83dd8a22d459be8337f547f7b9bc
camp-vfa <vikfand@gmail.com>
2016-06-28 14:05:45 +0200

    Configure SecurityConfigProvider to use Idp from path

--- proxy-config/src/main/java/no/difi/idporten/oidc/proxy/config/DefaultSecurityConfigProvider.java
+++ proxy-config/src/main/java/no/difi/idporten/oidc/proxy/config/DefaultSecurityConfigProvider.java
@@ -7,11 +7,16 @@ import no.difi.idporten.oidc.proxy.api.IdpConfigProvider;
 import no.difi.idporten.oidc.proxy.api.SecurityConfigProvider;
 import no.difi.idporten.oidc.proxy.model.HostConfig;
 import no.difi.idporten.oidc.proxy.model.IdpConfig;
+import no.difi.idporten.oidc.proxy.model.Path;
 import no.difi.idporten.oidc.proxy.model.SecurityConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Optional;
 
 public class DefaultSecurityConfigProvider implements SecurityConfigProvider {
+
+    Logger logger = LoggerFactory.getLogger(DefaultSecurityConfigProvider.class);
 
     private HostConfigProvider hostConfigProvider;
     private IdpConfigProvider idpConfigProvider;
@@ -25,11 +30,17 @@ public class DefaultSecurityConfigProvider implements SecurityConfigProvider {
 
     @Override
     public Optional<SecurityConfig> getConfig(String hostname, String path) {
-            HostConfig hostConfig = hostConfigProvider.getByHostname(hostname);
-            if (hostConfig == null)
-                return Optional.empty();
-            // or else should maybe give default config?
-            IdpConfig idpConfig = idpConfigProvider.getByIdentifier("idporten");
+        logger.debug("Getting config for {}{}", hostname, path);
+        HostConfig hostConfig = hostConfigProvider.getByHostname(hostname);
+        if (hostConfig == null)
+            return Optional.empty();
+        Optional<Path> pathOptional = hostConfig.getPathFor(path);
+        IdpConfig idpConfig;
+        if (pathOptional.isPresent()) {
+            idpConfig = idpConfigProvider.getByIdentifier(pathOptional.get().getIdp());
             return Optional.of(new TypesafeSecurityConfig(idpConfig, hostConfig));
+        } else {
+            return Optional.empty();
+        }
     }
 }
