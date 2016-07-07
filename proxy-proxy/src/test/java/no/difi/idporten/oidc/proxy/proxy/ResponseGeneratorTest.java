5078bc0fba0a2864cd4352a0ec758ba044740b62
camp-vfa <vikfand@gmail.com>
2016-07-07 09:40:24 +0200

    Make unit tests for ResponseGenerator using mocking

--- proxy-proxy/src/test/java/no/difi/idporten/oidc/proxy/proxy/ResponseGeneratorTest.java
+++ proxy-proxy/src/test/java/no/difi/idporten/oidc/proxy/proxy/ResponseGeneratorTest.java
@@ -3,6 +3,7 @@ package no.difi.idporten.oidc.proxy.proxy;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpResponse;
 import io.netty.handler.codec.http.HttpResponseStatus;
@@ -10,18 +11,22 @@ import no.difi.idporten.oidc.proxy.api.IdentityProvider;
 import no.difi.idporten.oidc.proxy.api.ProxyCookie;
 import no.difi.idporten.oidc.proxy.config.ConfigModule;
 import no.difi.idporten.oidc.proxy.lang.IdentityProviderException;
+import no.difi.idporten.oidc.proxy.model.DefaultProxyCookie;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mockito;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.nio.charset.Charset;
+import java.util.Date;
 import java.util.HashMap;
-
 
 
 public class ResponseGeneratorTest {
@@ -32,8 +37,16 @@ public class ResponseGeneratorTest {
 
     private ResponseGenerator responseGenerator;
 
+    /**
+     * This one can capture and show you which arguments were passed to methods you spy on.
+     */
     @Captor
-    ArgumentCaptor<HttpResponse> httpResponseCaptor;
+    private ArgumentCaptor<FullHttpResponse> httpResponseCaptor;
+    /**
+     * Acts as an empty ChannelHandlerContext object, but can be configured to return any value you want from any of
+     * its methods if needed. We use the httpResponseCaptor to see which arguments writeAndFlush were called with.
+     */
+    @Mock
     private ChannelHandlerContext ctxMock;
 
 
@@ -42,11 +55,20 @@ public class ResponseGeneratorTest {
         Injector injector = Guice.createInjector(new ConfigModule(), new ProxyModule());
 
         this.host = "not.configured.host";
-
-        this.ctxMock = Mockito.mock(ChannelHandlerContext.class);
-        MockitoAnnotations.initMocks(this);
-
         this.responseGenerator = new ResponseGenerator();
+
+        // Instantiating mock
+        this.ctxMock = Mockito.mock(ChannelHandlerContext.class);
+        MockitoAnnotations.initMocks(this); // Needed for httpResponseCaptor to work
+
+    }
+
+    /**
+     * Should reset mocks here, but for some reason the mock is not reset between tests...
+     */
+    @AfterTest
+    public void cleanUpMocks() {
+        Mockito.reset(ctxMock); // Don't think this works
     }
 
     @Test
@@ -54,18 +76,27 @@ public class ResponseGeneratorTest {
         Assert.assertNotNull(responseGenerator);
     }
 
-    // We need more sophisticated Mockito.mocking to do these tests as they actually write to a Netty channel
     @Test
     public void generateRedirectResponseWhenIdentityProviderNotConfigured() throws Exception {
+        Mockito.reset(ctxMock); // It only works when resetting here
+
         IdentityProvider identityProviderMock = Mockito.mock(IdentityProvider.class);
+
+        // The spy is basically the real responseGenerator objects, but it watches which methods have been called
+        // and how many times each method has been called.
         ResponseGenerator responseGeneratorSpy = Mockito.spy(responseGenerator);
+        // Because of exceptions we need to wrap tests in these ugly try/catch things
         try {
             responseGeneratorSpy.generateRedirectResponse(ctxMock, identityProviderMock);
         } catch (NullPointerException exc) {
 
         } finally {
+            // All we need to test here is that the correct methods have been called and maybe which arguments
+            // they were called with.
             Mockito.verify(responseGeneratorSpy).generateRedirectResponse(Mockito.any(), Mockito.any());
-            /*
+            Mockito.verify(identityProviderMock).generateRedirectURI();
+
+            /* Not sure whether it's this method's responsibility to create an error when this goes south.
             Mockito.verify(ctxMock).writeAndFlush(httpResponseCaptor.capture());
             Mockito.verify(responseGeneratorSpy).generateDefaultResponse(Mockito.any(), Mockito.anyString());
 
@@ -78,6 +109,7 @@ public class ResponseGeneratorTest {
 
     @Test
     public void generateRedirectResponseWhenIdentityProviderIsConfigured() throws Exception {
+        Mockito.reset(ctxMock); // It only works when resetting here
         String validRedirectUrl = "valid.redirect.url";
         IdentityProvider identityProviderMock = Mockito.mock(IdentityProvider.class);
         ResponseGenerator responseGeneratorSpy = Mockito.spy(responseGenerator);
@@ -89,6 +121,7 @@ public class ResponseGeneratorTest {
         } finally {
             Mockito.verify(responseGeneratorSpy).generateRedirectResponse(Mockito.any(), Mockito.any());
             Mockito.verify(identityProviderMock).generateRedirectURI();
+            Mockito.atLeastOnce();
             Mockito.verify(ctxMock).writeAndFlush(httpResponseCaptor.capture());
 
             HttpResponse actual = httpResponseCaptor.getValue();
@@ -101,32 +134,60 @@ public class ResponseGeneratorTest {
 
     @Test
     public void generateDefaultResponse() {
+        Mockito.reset(ctxMock); // It only works when resetting here
+        ResponseGenerator responseGeneratorSpy = Mockito.spy(responseGenerator);
         try {
-            responseGenerator.generateDefaultResponse(ctxMock, host);
+            responseGeneratorSpy.generateDefaultResponse(ctxMock, host);
         } catch (NullPointerException exc) {
 
         } finally {
-            Mockito.verify(ctxMock).writeAndFlush(Mockito.any(HttpResponse.class));
+            Mockito.verify(responseGeneratorSpy).generateDefaultResponse(Mockito.any(), Mockito.anyString());
+            Mockito.verify(ctxMock).writeAndFlush(httpResponseCaptor.capture());
+
+            FullHttpResponse actual = httpResponseCaptor.getValue();
+            Assert.assertTrue(actual instanceof HttpResponse);
+            String content = actual.content().toString(Charset.forName("UTF-8"));
+            Assert.assertEquals(actual.status(), HttpResponseStatus.BAD_REQUEST);
+            Assert.assertTrue(actual.headers().getAsString(HttpHeaderNames.CONTENT_TYPE).contains(ResponseGenerator.TEXT_HTML));
+            Assert.assertTrue(content.contains(host));
         }
     }
 
     @Test
-    public void generateJWTResponse() {
-        ProxyCookie proxyCookieMock = Mockito.mock(ProxyCookie.class);
+    public void generateJWTResponseWhenCorrectlyConfigured() throws Exception {
+        Mockito.reset(ctxMock); // It only works when resetting here
+        ResponseGenerator responseGeneratorSpy = Mockito.spy(responseGenerator);
+
+        String uuid = "uuid";
+        String cookieName = "TESTCOOKIE";
+        String path = "/beskyttet-side";
+        Date farFutureDate = new Date(new Date().getTime() + Integer.MAX_VALUE);
 
         String pid = "08023549930";
         HashMap<String, String> userData = new HashMap<>();
         userData.put("pid", pid);
         userData.put("tokenType", "JWTToken");
         userData.put("aud", "dificamp");
+        ProxyCookie proxyCookie =  new DefaultProxyCookie(uuid, cookieName, host, path, farFutureDate, farFutureDate, userData);
         try {
-            responseGenerator.generateJWTResponse(ctxMock, userData, proxyCookieMock);
+            responseGeneratorSpy.generateJWTResponse(ctxMock, userData, proxyCookie);
         } catch (NullPointerException exc) {
 
         } catch (IdentityProviderException exc) {
 
         } finally {
-            Mockito.verify(ctxMock).writeAndFlush(Mockito.any());
+            Mockito.verify(responseGeneratorSpy).generateJWTResponse(Mockito.any(), Mockito.any(), Mockito.any());
+            Mockito.verify(ctxMock).writeAndFlush(httpResponseCaptor.capture());
+
+            FullHttpResponse actual = httpResponseCaptor.getValue();
+            Assert.assertTrue(actual instanceof HttpResponse);
+            String content = actual.content().toString(Charset.forName("UTF-8"));
+            Assert.assertEquals(actual.status(), HttpResponseStatus.OK);
+            Assert.assertTrue(actual.headers().getAsString(HttpHeaderNames.CONTENT_TYPE).contains(ResponseGenerator.APPLICATION_JSON));
+            userData.entrySet().stream().forEach(entry -> {
+                Assert.assertTrue(content.contains(entry.getKey()));
+                Assert.assertTrue(content.contains(entry.getValue()));
+            });
         }
     }
 }
