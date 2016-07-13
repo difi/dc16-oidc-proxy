b19077d3db347a227504a48edfaa58881d2c087c
viktorfa <vikfand@gmail.com>
2016-07-13 14:10:10 +0200

    Printing stack trace when an exception occurs in a Netty Handler Adapter to avoid cryptic messages

--- proxy-proxy/src/main/java/no/difi/idporten/oidc/proxy/proxy/AbstractHandlerAdapter.java
+++ proxy-proxy/src/main/java/no/difi/idporten/oidc/proxy/proxy/AbstractHandlerAdapter.java
@@ -14,6 +14,7 @@ abstract class AbstractHandlerAdapter extends ChannelInboundHandlerAdapter {
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
         logger.warn(cause.getMessage(), cause);
         closeOnFlush(ctx.channel());
     }
