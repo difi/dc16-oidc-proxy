5f9aba3547cb353fffaa19f7a7f709aba60c56b6
viktorfa <vikfand@gmail.com>
2016-07-29 12:15:13 +0200

    Implement simple memoization of results in SecurityConfigProvider

--- proxy-config/src/main/java/no/difi/idporten/oidc/proxy/config/DefaultSecurityConfigProvider.java
+++ proxy-config/src/main/java/no/difi/idporten/oidc/proxy/config/DefaultSecurityConfigProvider.java
@@ -6,7 +6,9 @@ import no.difi.idporten.oidc.proxy.api.IdpConfigProvider;
 import no.difi.idporten.oidc.proxy.api.SecurityConfigProvider;
 import no.difi.idporten.oidc.proxy.model.SecurityConfig;
 
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class DefaultSecurityConfigProvider implements SecurityConfigProvider {
 
@@ -14,18 +16,28 @@ public class DefaultSecurityConfigProvider implements SecurityConfigProvider {
 
     private IdpConfigProvider idpConfigProvider;
 
+    private Map<String, Optional<SecurityConfig>> memoizationCache;
+
     @Inject
     public DefaultSecurityConfigProvider(HostConfigProvider hostConfigProvider, IdpConfigProvider idpConfigProvider) {
         this.hostConfigProvider = hostConfigProvider;
         this.idpConfigProvider = idpConfigProvider;
+        this.memoizationCache = new ConcurrentHashMap<>();
     }
 
 
     @Override
     public Optional<SecurityConfig> getConfig(String hostname, String path) {
+        return memoizationCache.computeIfAbsent(hostname + path, this::privateGetConfig);
+    }
+
+    private Optional<SecurityConfig> privateGetConfig(String url) {
+        String hostname = url.substring(0, url.indexOf('/'));
+        String path = url.substring(url.indexOf('/'), url.length());
         if (hostConfigProvider.getByHostname(hostname) == null) {
             return Optional.empty();
+        } else {
+            return Optional.of(new DefaultSecurityConfig(hostname, path, hostConfigProvider, idpConfigProvider));
         }
-        return Optional.of(new DefaultSecurityConfig(hostname, path, hostConfigProvider, idpConfigProvider));
     }
 }
